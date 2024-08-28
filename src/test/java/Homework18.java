aa9ed4dd4176e2d46c0e67eab48aaa080f855cfe
Juhkri <152635954+Juhkri@users.noreply.github.com>
2024-08-28 15:20:55 -0400

    Update Homework18.java

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,1 +1,15 @@
+public class Homework18 extends BaseTest {
 
+  public void playNextSong {
+    WebElement play = driver.findElement(By.cssSelector("[title='Play or resume']"));
+    play.click();
+  }
+    
+  public void verifySongPlaying {
+  assert data-testid="pause-btn"
+    
+
+    @Test
+  public void playSong() throws InterruptedException {
+    [title="Play or resume"]
+    
