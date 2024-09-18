6b778313a7f21225c561652356ac96ed55392db8
Juhkri <juhkri@hotmail.com>
2024-09-18 04:17:15 -0400

    Completed HW19

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,9 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -14,7 +17,9 @@ public class BaseTest {
     WebDriver driver;
     private String email;
     private String password;
+    private String song;
     String url = "https://qa.koel.app/";
+    WebDriver wait;
 
     public void verifyPlay() {
         WebElement soundbar = driver.findElement(By.cssSelector("div[data-testid='sound-bar-play']"));
@@ -61,6 +66,56 @@ public class BaseTest {
         inputEmail();
         inputPassword();
         loginButton();
+    }
+
+    public void inputSearch() {
+        WebElement searchBar = driver.findElement(By.cssSelector("input[type='search']"));
+        searchBar.clear();
+        searchBar.sendKeys(this.song);
+    }
+
+    public void searchSong(String song) {
+        this.song = song;
+    }
+
+    public void viewAllButton() {
+        WebElement viewAll = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
+        viewAll.click();
+    }
+
+    public void firstSongSelect() {
+        WebElement firstSong = driver.findElement(By.cssSelector("#songResultsWrapper > div > div > div.item-container > table > tr:nth-child(1)"));
+        firstSong.click();
+    }
+
+    public void addToPlaylist() {
+        WebElement addTo = driver.findElement(By.cssSelector("[data-test='add-to-btn']"));
+        addTo.click();
+    }
+
+    public void selectPlaylist() {
+        WebElement selectPlaylist = driver.findElement(By.cssSelector("[#songResultsWrapper > header > div.song-list-controls > div > section.existing-playlists > ul > li.playlist]"));
+        selectPlaylist.click();
+    }
+
+
+    public void createPlaylist() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement createPlaylistButton = driver.findElement(By.cssSelector("[data-testid=\"sidebar-create-playlist-btn\"]"));
+        createPlaylistButton.click();
+        WebElement newPlaylistButton = driver.findElement(By.cssSelector("[data-testid='playlist-context-menu-create-simple']"));
+        newPlaylistButton.click();
+        WebElement newPlaylistName = driver.findElement(By.cssSelector("#playlists > form > input[type=text]"));
+        newPlaylistName.clear();
+        newPlaylistName.sendKeys("Homework");
+        newPlaylistName.sendKeys(Keys.ENTER);
+    }
+
+    public void deleteFirstPlaylist() {
+        WebElement firstPlaylist = driver.findElement(By.cssSelector("[class=\"playlist playlist\"]"));
+        firstPlaylist.click();
+        WebElement deletePlaylistButton = driver.findElement(By.cssSelector("[class=\"del btn-delete-playlist\"]"));
+        deletePlaylistButton.click();
     }
 
     @BeforeMethod
