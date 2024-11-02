80396de6337be4f48d4057f3c7f6b9908cd47e3d
Juhkri <juhkri@hotmail.com>
2024-11-02 14:04:26 -0400

    Successful Test for Homework29

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,65 +1,59 @@
 import Pages.HomePage;
 import Pages.LoginPage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertFalse;
 
 public class LoginTests extends BaseTest {
 
-//    @Test
-//    public void loginEmptyEmailPassword() {
-//    setEmailPassword("", "");
-//    inputEmail();
-//    inputPassword();
-//    loginButton();
-//    Assert.assertEquals(driver.getCurrentUrl(),"https://qa.koel.app/");
-//}
+    @Test
+    public void loginEmptyEmailPassword() {
+    setEmailPassword("", "");
+    inputEmail();
+    inputPassword();
+    loginButton();
+    Assert.assertEquals(driver.getCurrentUrl(),"https://qa.koel.app/");
+}
 
-    // Login Test using POM
-
-//    @Test
-//    public void positiveLoginTest(){
-//        LoginPage loginPage = new LoginPage(driver);
-//        HomePage homePage = new HomePage(driver);
-//
-//        loginPage.provideEmail("kristofer.juhasz@testpro.io");
-//        loginPage.providePassword("Logintest1!");
-//        loginPage.clickSubmit();
-//
-//        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
-//    }
-
-    // Login Test using PF
+//     Login Test using POM
 
     @Test
-    public void positiveLoginTestPF() throws InterruptedException {
-        LoginPage loginPage = new LoginPage(getDriver());
-        HomePage homePage = new HomePage(getDriver());
+    public void positiveLoginTest(){
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
 
-        // Login
-        loginPage.provideEmailPF("kristofer.juhasz@testpro.io");
-        loginPage.providePasswordPF("Logintest1!");
-        loginPage.clickSubmitBtnPF();
+        loginPage.provideEmail("kristofer.juhasz@testpro.io");
+        loginPage.providePassword("Logintest1!");
+        loginPage.clickSubmit();
 
         Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
     }
 
-    @Test
-    public void negativeLoginTestPF() throws InterruptedException {
-        LoginPage loginPage = new LoginPage(getDriver());
-        HomePage homePage = new HomePage(getDriver());
+//     Login Test using PF
 
-        // Login
-        loginPage.provideEmailPF("wrongusername@testpro.io");
-        loginPage.providePasswordPF("Logintest");
-        loginPage.clickSubmitBtnPF();
+//    @Test
+//    public void positiveLoginTestPF() throws InterruptedException {
+//        LoginPage loginPage = new LoginPage(getDriver());
+//        HomePage homePage = new HomePage(getDriver());
+//
+//        // Login
+//        loginPage.provideEmailPF("kristofer.juhasz@testpro.io");
+//        loginPage.providePasswordPF("Logintest1!");
+//        loginPage.clickSubmitBtnPF();
+//
+//        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+//    }
+//
+//    @Test
+//    public void negativeLoginTestPF() throws InterruptedException {
+//        LoginPage loginPage = new LoginPage(getDriver());
+//        HomePage homePage = new HomePage(getDriver());
+//
+//        // Login
+//        loginPage.provideEmailPF("wrongusername@testpro.io");
+//        loginPage.providePasswordPF("Logintest");
+//        loginPage.clickSubmitBtnPF();
+//
+//        Assert.assertEquals(getDriver().getCurrentUrl(), url);
+//    }
 
-        Assert.assertEquals(getDriver().getCurrentUrl(), url);
-    }
 }
